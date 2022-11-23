017c1f928cfcbd1eac28905908bc11002a59fe0a
Brenda Brito <bbritob8@gmail.com>
2022-11-23 17:24:53 -0300

    creation Publication Repository

--- Doae-cs/src/Repositories/Interfaces/IPublicationRepository.cs
+++ Doae-cs/src/Repositories/Interfaces/IPublicationRepository.cs
@@ -5,7 +5,7 @@ namespace Doae.Repositories.Interfaces
     public interface IPublicationRepository
     {
         Task<PublicationModel?>FindPublicationById(int id);
-        Task<List<PublicationModel>>FindPublicationByIdInstitution(int nstitutionId);
+        Task<List<PublicationModel>>FindPublicationByIdInstitution(int institutionId);
         Task<List<PublicationModel>>FindAllPublications();
         Task<PublicationModel> CreatePublication(PublicationModel publication);
         Task<bool?> DeletePublication(int id);
---
+++ Doae-cs/src/Repositories/PublicationRepository.cs
@@ -0,0 +1,54 @@
+using Doae.Repositories.Interfaces;
+using Doae.database;
+using Doae.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doae.Repositories
+{
+    public class PublicationRepository : IPublicationRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public PublicationRepository(ApplicationDBContext applicationDBContext)
+        {
+            _dbContext = applicationDBContext;
+        }
+
+        public async Task<List<PublicationModel>> FindAllPublications()
+        {
+            return await _dbContext.Publications.ToListAsync();
+        }
+
+        public async Task<PublicationModel?> FindPublicationById(int id)
+        {
+            return await _dbContext.Publications.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<PublicationModel>> FindPublicationByIdInstitution(int institutionId)
+        {
+            return await _dbContext.Publications.Where(x => x.InstitutionId == institutionId).ToListAsync();
+        }
+
+        public async Task<PublicationModel> CreatePublication(PublicationModel publication)
+        {
+            await _dbContext.Publications.AddAsync(publication);
+            await _dbContext.SaveChangesAsync();
+
+            return publication;
+        }
+
+        public async Task<bool?> DeletePublication(int id)
+        {
+            PublicationModel? publicationById = await FindPublicationById(id);
+
+            if (publicationById == null)
+            {
+                throw new Exception($"Publicação para o ID:{id} não foi encontrado no banco de dados");
+            }
+
+            _dbContext.Publications.Remove(publicationById);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
